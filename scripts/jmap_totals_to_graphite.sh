3b2bee984ae0ed563c27083083b1f4d96ddba0bc
massiccio <michelemazzucco@gmail.com>
2017-06-07 16:01:05 +0200

    Adding bash script collecting the size of java objects

---
+++ scripts/jmap_totals_to_graphite.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Note: the above script causes a full GC at every execution, see
+# http://netflix.github.io/spectator/en/latest/ext/jvm-gc-causes/#heap_inspection_initiated_gc
+
+HOSTNAME=`hostname --short`
+GRAPHITE=127.0.0.1
+GRAPHITE_PORT=2003
+
+get_total_heap_values()
+{
+	DATE=`date +%s`
+	pid=`jps |grep jar`
+        if [[ ! -z "$pid" ]]; then
+		echo $pid | cut -d" " -f1| xargs jmap -histo|grep Total|sed 's/\s\+/ /g'|cut -d" " -f2,3,4|
+                awk '{print "ngserver.'$HOSTNAME'.heap.total.objects "$1"\nngserver.'$HOSTNAME'.heap.total.object_size_bytes " $2}' | while read line
+                do
+                        echo $line $DATE | nc $GRAPHITE $GRAPHITE_PORT
+                done
+
+                echo $pid | cut -d" " -f1| xargs jmap -histo:live|grep Total|sed 's/\s\+/ /g'|cut -d" " -f2,3,4|
+                awk '{print "ngserver.'$HOSTNAME'.heap.total.live_objects "$1"\nngserver.'$HOSTNAME'.heap.total.live_objects_size_bytes " $2}' | while read line
+                do
+                        echo $line $DATE | nc $GRAPHITE $GRAPHITE_PORT
+                done
+        fi
+
+}
+
+
+while true; do
+	START=$(date +%s);
+        get_total_heap_values
+	END=$(date +%s);
+	DURATION=$((END-START))
+	SLEEP_FOR=$((10-DURATION));
+	if [ $SLEEP_FOR > 0 ]; then
+		sleep $SLEEP_FOR
+	fi
+done
